973490a4fd302767035e41c8951546d2c3e63d1c
vr97 <v_rao97@yahoo.com>
2017-03-11 18:45:27 +0530

    Update 54.js

--- test1/54.js
+++ test1/54.js
@@ -1,10 +1,12 @@
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
+    var name;
+    var n;
     
     function sayHello (event) {
       this.textContent = "Said it!";
-      var name =
+       name =
        document.getElementById("name").value;
        var message = "<h2>Hello " + name + "!</h2>";
 
@@ -24,14 +26,36 @@ document.addEventListener("DOMContentLoaded",
       }
     }
 
+    function sayhi(event){
+       n =
+       document.getElementById("name2").value;
+       var m = "<h2>Hello " + n + "!</h2>";
+
+      document
+        .getElementById("content2")
+        .innerHTML = m;
+
+        if (name === n) {
+        var title = 
+          document
+            .querySelector("#title")
+            .textContent;
+        title = "SAME STRINGS !!!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+
+
+
+    }
+
     // Unobtrusive event binding
     document.querySelector("button")
       .addEventListener("click", sayHello);
 
+      document.querySelector("#two")
+        .addEventListener("click",sayhi);
+
   }
 );
-
-
-
-// document.querySelector("button")
-//   .onclick = sayHello;
